01e82f73a465b802eb30bf743b5cdcfcfeef8a3e
Marius Van Nieuwenhuyse <marius.van-nieuwenhuyse@epitech.eu>
2018-05-23 21:35:08 +0200

    file loading

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let app = undefined;
+let map_container = undefined;
+let sector = undefined;
 
 WebFont.load({
 	google: {
 		families: ['Roboto Mono']
 	},
 	active:e=>{
-		console.log("font loaded!");
 		init();
 	}
 });
@@ -27,6 +28,37 @@ function start_pixijs()
 		app.renderer.resize(window.innerWidth, window.innerHeight - 4);
 	})
 	document.body.appendChild(app.view);
+	map_container = new PIXI.Container();
+	app.stage.addChild(map_container);
+}
+
+function load_sector(airport_icao)
+{
+	$.getJSON('Sectors/' + airport_icao + '.json', function(json) {
+		sector = json;
+		draw_sector();
+		console.log(json);
+	})
+}
+
+function draw_sector()
+{
+	console.log("draw sectors");
+	if (!sector)
+		return;
+	console.log("there is sector");
+	for (var i = 0; i < sector.map.length; i++) {
+		console.log("line");
+		var line = sector.map[i];
+		var line_g = new PIXI.Graphics();
+		line_g.lineStyle(1, 0xffffff, 1);
+		line_g.moveTo(line[0].x, line[0].y);
+		for (var j = 1; j < line.length; j++) {
+			line_g.lineTo(line[j].x, line[j].y);
+			console.log(line[j].x + ", " + line[j].y)
+		}
+		map_container.addChild(line_g);
+	}
 }
 
 function draw_gui(ctrl_pos)
@@ -50,6 +82,7 @@ function draw_gui(ctrl_pos)
 
 function init()
 {
+	load_sector("LFMT");
 	start_pixijs();
 	draw_gui("LFMT_GND");
 }
